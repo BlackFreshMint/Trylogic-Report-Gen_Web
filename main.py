297323f52d630125ab9c1f9e685d43fa6b48edc3
BlackFreshMint <freshmint2511@gmail.com>
2024-12-03 05:05:37 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, jsonify
+import os
 import subprocess
 
 app = Flask(__name__)
@@ -26,3 +27,5 @@ def view_data():
 
 if __name__ == '__main__':
     app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)  # Bind to 0.0.0.0 and use the correct port
